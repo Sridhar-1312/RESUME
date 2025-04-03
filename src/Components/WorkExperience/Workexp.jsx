1bc2a25b86d015ab285a971a120ad592ac0d24d3
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-04-03 11:55:18 +0530

    modified2

---
+++ src/Components/WorkExperience/Workexp.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Workexp() {
+  return (
+<>
+<div className="container">
+    {/* first row */}
+    <div className="first-row">
+        <h1 className='a'>+ Work Experience</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ab quibusdam iusto ipsam dicta delectus a adipisci cumque mollitia enim, fugit accusamus, exercitationem doloribus voluptate facilis beatae quas repellendus voluptas.</p>
+    </div>
+    {/* secondrow */}
+    <div className="secondrow">
+        <div className="row">
+            <div className="col-6">
+            jkfh
+            </div>
+            <div className="col-6">
+            ..khhkh
+            </div>
+        </div>
+    </div>
+    
+</div>
+</>  )
+}
+
+export default Workexp
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import  'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import Navbar from './Navbar.jsx'
+import Navbar from './Components/Navbar/Navbar'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar/>
+    <Navbar />
   </StrictMode>,
 )
